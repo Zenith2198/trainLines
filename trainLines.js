502088a49829581337b14f828f5ac530399506c9
Carter Rollins <croll2198@gmail.com>
2024-08-29 18:15:28 -0400

    Adds ask function

---
+++ trainLines.js
@@ -0,0 +1,14 @@
+//setting up async/await ask functionality for the command line
+const readline = require("readline");
+const readlineInterface = readline.createInterface(
+    process.stdin,
+    process.stdout
+);
+
+function ask(questionText) {
+    return new Promise((resolve, reject) => {
+        readlineInterface.question(questionText, resolve);
+    });
+}
+
+//YOUR CODE GOES HERE
